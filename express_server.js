19be23c1d1e028d9df383d7c9a0f585970ca4108
Nat York <natyork@gmail.com>
2016-10-13 02:53:52 +0000

    added error handling and  cleaned up comments, still need to fix CRUD layout, further clean-up and add some styling at some point

--- express_server.js
+++ express_server.js
@@ -1,92 +1,118 @@
 // Welcome to the Wee App Server...
 
-// Dependencies
+// DEPENDENCIES
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-// Configuration
+// CONFIGURATION
 app.set("view engine", "ejs");
 const PORT = process.env.PORT || 8080;
 
-// Middleware
+// MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Constants
-
+// CONSTANTS
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// ROUTES
 
-// Routes
-app.get("/", (req, res) => { //redirects to /urls
+//redirects to /urls
+app.get("/", (req, res) => {
   res.redirect(303, "/urls");
 });
 
-app.get("/urls", (req, res) => { //renders urls_index
+
+// renders urls_index
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => { //renders url_new
+
+// renders url_new
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => { //post from form on /urls/new
+
+// post from create form in /urls/new, updates urlDatabase, then redirects to urls/:id
+// includes error handling for:
+//  -duplicate shortURL
+//  -add protocol to longURL if not included by user
+app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = "weee";
+
   while(urlDatabase[shortURL]) {
-    console.log(shortURL);
     shortURL = generateRandomString();
-  }// error handling for a duplicate shortURL
+  }
+
   var protocol = /http:\/\//
   if (protocol.test((req.body).longURL) === true){
-    urlDatabase[shortURL] = (req.body).longURL; //updates urlDatabase with new shortURL: longURL
+    urlDatabase[shortURL] = (req.body).longURL;
   } else {
     urlDatabase[shortURL] = "http://" + (req.body).longURL;
-  }// if user does not enter http://  need to add http://
-  //if user does enter http://just use longURL
-  res.redirect(303, `http://localhost:8080/urls/${shortURL}`); //redirects to urls/:id
+  }
+
+  res.redirect(303, `http://localhost:8080/urls/${shortURL}`);
 });
 
-app.post("/urls/:id/delete", (req, res) => { //post from delete form on urls
+
+// post from delete form on urls, deletes URL entry from urLDatabase, then redirects to /urls
+app.post("/urls/:id/delete", (req, res) => {
   let templateVars = {shortURL: req.params.id};
-  delete urlDatabase[templateVars.shortURL]; //deletes shortUrl/LongURL key value pair from urlDatabase
-  res.redirect(303,"/urls"); //redirects to /urls
+  delete urlDatabase[templateVars.shortURL];
+  res.redirect(303,"/urls");
 });
 
-app.get("/urls/:id", (req, res) => { //renders urls_show
+
+// renders urls_show, includes error handling if shortURL does not exist
+// TODO: clean up all the variables that were used to break down the problem
+app.get("/urls/:id", (req, res) => {
   let shorty = req.params.id;
   let longy = urlDatabase[shorty];
   let templateVars = { "shortURL": shorty, "longy": longy };
   if(!urlDatabase[shorty]){
     res.status(404).send('Whoopsie! The site you are looking for can\'t be found so instead you get this wee 404 message');
   } else {
-    res.render("urls_show", templateVars); //at this point and user must include http:// in front of the web address they enter in order for this to work
+    res.render("urls_show", templateVars);
   }
 });
 
-app.get("/u/:id", (req, res) => { //redirects to long URL corresponding to :id
+
+// redirects to long URL corresponding to :id
+app.get("/u/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   if(!urlDatabase[templateVars.shortURL]){
     res.status(404).send('Whoopsie! The site you are looking for can\'t be found so instead you get this wee 404 message');
   } else {
-    res.redirect(303, urlDatabase[templateVars.shortURL]); //at this point and user must include http:// in front of the web address they enter in order for this to work
+    res.redirect(303, urlDatabase[templateVars.shortURL]);
   }
 });
 
-app.post("/urls/:id", (req, res) => { //post from update form on urls_show
+// post from update form on urls_show, updates urlDatabase and redirects to /urls
+// includes error handling for:
+//  -form submitted with empty field
+//  -add protocol to longURL if not included by user
+app.post("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
-  urlDatabase[templateVars.shortURL] = (req.body).longURL; //updates urlDatabase @ exisiting shortURL with new longURL
-  // need to add error handling if update field is empty
-  res.redirect(303, "/urls");
+  if (((req.body).longURL) !== "") {
+    var protocol = /http:\/\//
+    if (protocol.test((req.body).longURL) === true){
+      urlDatabase[templateVars.shortURL] = (req.body).longURL;
+    } else {
+      urlDatabase[templateVars.shortURL] = "http://" + (req.body).longURL;
+    }
+    res.redirect(303, "/urls");
+  }
 });
 
-// Edit
+// EDIT
 
-// Delete
+// DELETE
 
 
 app.listen(PORT, () => {
